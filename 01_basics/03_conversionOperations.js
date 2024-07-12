a95a3f0a72d3559763d925df3f8cd124e601dc61
Madhvi Bhatnagar <madhvibhatnagar@gmail.com>
2024-07-12 20:17:55 +0000

    conversion and comparison

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,24 +1,49 @@
-let score = "Madhvi";
-console.log(typeof score)
-console.log(typeof (score))
+// let score = "Madhvi";
+// console.log(typeof score)
+// console.log(typeof (score))
 
-let valueInNumber = Number (score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber); //Nan -
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); //Nan -
 
-//"33" => 33
-// "33hd" => NaN => type - number
-// true => 1; false => 0
-// 1=> true; 0 => false
-//"" => false
-// "Madhvi" => true
+// // "33" => 33
+// // "33hd" => NaN => type - number
+// // true => 1; false => 0
+// // 1=> true; 0 => false
+// // "" => false
+// // "Madhvi" => true
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// /**********operations************* */
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2%3);
+
+// ///string
+// let str1 = "hello"
+// let str2 = "Madhvi"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(("1" +2 + 2));
+// console.log(1 + 2 + "3");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter =100
+// gameCounter++;
+++gameCounter
+console.log(gameCounter);
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Javascript-Learning
 code repo for javascript
 node 01_basics/03_conversionOperations.js
-node 01_basics/03_conversionOperations.js
+node 01_basics/04_comparison.js
 node 01_basics/03_conversionOperations.js
 node 01_basics/03_conversionOperations.js
 node 01_basics/03_conversionOperations.js
